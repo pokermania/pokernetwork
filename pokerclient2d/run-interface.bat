075f4628ac49209e4f9a50a32383103d00cdd045
bartok <bartok@c4f34931-41c7-4605-b615-9bb9f479c57e>
2006-02-06 16:20:57 +0000

    cygwin
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@885 c4f34931-41c7-4605-b615-9bb9f479c57e

---
+++ pokerclient2d/run-interface.bat
@@ -0,0 +1,2 @@
+set ROOT=/cygdrive/u/new_pok3d
+poker3d-interface.exe --glade %ROOT%/data.proprio/interface/interface.glade --datadir %ROOT%/data.proprio -r %ROOT%/data.proprio/interface/gtkrc --smiley %ROOT%/data.proprio/interface/smileys --verbose 6 --display :1 --port 19379
