66f4cd1701acc6fee233058ca0c103f5f45820cf
loic <loic@c4f34931-41c7-4605-b615-9bb9f479c57e>
2006-03-22 18:54:59 +0000

    pot limit
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@963 c4f34931-41c7-4605-b615-9bb9f479c57e

--- pokernetwork/pokerclient.py
+++ pokernetwork/pokerclient.py
@@ -545,9 +545,7 @@ class PokerClientProtocol(UGAMEClientProtocol):
             ( serial, amount ) = args
             player = game.getPlayer(serial)
             last_action = game.historyGet()[-1][0]
-            pot_limit = game.betInfo()['max'] == "pot"
-            if ( last_action == "raise" or
-                 ( pot_limit and last_action == "call" ) ) :
+            if ( last_action == "raise" or last_action == "call" ) :
                 if not self.no_display_packets:
                     forward_packets.extend(self.updateBetLimit(game))
                 forward_packets.append(PacketPokerHighestBetIncrease(game_id = game.id))
