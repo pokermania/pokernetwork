25ec438a9d887fae59e2073c2d1932af9a0e513f
loic <loic@c4f34931-41c7-4605-b615-9bb9f479c57e>
2006-11-30 08:12:32 +0000

    skin_image is a text instead of blob because it's a lot easier to handle in various languages
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@1903 c4f34931-41c7-4605-b615-9bb9f479c57e

--- database/1.0.30-1.0.31.sql
+++ database/1.0.30-1.0.31.sql
@@ -10,8 +10,12 @@ UPDATE server SET version = '1.0.31';
 -- 
 -- Money is now a 64 bit value
 --
-ALTER TABLE `user2money` CHANGE COLUMN `amount` `amount` BIGINT not null;
-ALTER TABLE `user2money` CHANGE COLUMN `rake` `rake` BIGINT default 0 not null;
-ALTER TABLE `user2money` CHANGE COLUMN `points` `points` BIGINT default 0 not null;
-ALTER TABLE `safe` CHANGE COLUMN `value` `value` BIGINT not null;
-ALTER TABLE `counter` CHANGE COLUMN `value` `value` BIGINT not null;
+ALTER TABLE `user2money` CHANGE COLUMN `amount` `amount` BIGINT NOT NULL;
+ALTER TABLE `user2money` CHANGE COLUMN `rake` `rake` BIGINT DEFAULT 0 NOT NULL;
+ALTER TABLE `user2money` CHANGE COLUMN `points` `points` BIGINT DEFAULT 0 NOT NULL;
+ALTER TABLE `safe` CHANGE COLUMN `value` `value` BIGINT NOT NULL;
+ALTER TABLE `counter` CHANGE COLUMN `value` `value` BIGINT NOT NULL;
+---
+--- Image is stored in text form
+---
+ALTER TABLE `users` CHANGE COLUMN `skin_image` `skin_image` TEXT NOT NULL;
