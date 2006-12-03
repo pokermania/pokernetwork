14f09a1c3100755460c8f77e8eab605bb5722717
loic <loic@c4f34931-41c7-4605-b615-9bb9f479c57e>
2006-12-03 18:20:07 +0000

    messages table
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@1987 c4f34931-41c7-4605-b615-9bb9f479c57e

---
+++ database/1.0.31-1.0.32.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS messages;
+
+CREATE TABLE messages (
+  serial INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  send_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  message TEXT DEFAULT '',
+  sent CHAR DEFAULT 'n',
+
+  PRIMARY KEY (serial, send_date, sent)
+)  ENGINE=InnoDB CHARSET=utf8;
