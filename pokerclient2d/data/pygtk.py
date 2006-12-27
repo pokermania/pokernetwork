aa871d096a3232207476f0db9a857a8bbddfeccb
proppy <proppy@c4f34931-41c7-4605-b615-9bb9f479c57e>
2006-12-27 18:16:17 +0000

    added sample pygtk application using new skin
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@2183 c4f34931-41c7-4605-b615-9bb9f479c57e

---
+++ pokerclient2d/data/pygtk.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- mode: python -*-
+#
+# Copyright (C) 2006 Mekensleep
+#
+# Mekensleep
+# 24 rue vieille du temple
+# 75004 Paris
+#       licensing@mekensleep.com
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Authors:
+#  Johan Euphrosine <johan@mekensleep.com>
+#
+
+import gtk
+import gtk.glade
+if __name__ == '__main__':
+    gtk.rc_parse("skin/gtkrc")
+    glade = gtk.glade.XML('skin/mockup.glade')
+    window = glade.get_widget('game_window')
+    window.show_all()
+    gtk.main()
+
+# Interpreted by emacs
+# Local Variables:
+# compile-command: "make skin/gtkrc skin/mockup.glade &&  /usr/bin/python pygtk.py"
+# End:
