48ff8fccd840930b0675707542f05194774d2320
proppy <proppy@c4f34931-41c7-4605-b615-9bb9f479c57e>
2007-01-15 14:06:32 +0000

    dont' remove sit_seat
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@2315 c4f34931-41c7-4605-b615-9bb9f479c57e

--- pokerclient2d/gamewindow.py
+++ pokerclient2d/gamewindow.py
@@ -43,7 +43,7 @@ class GameWindowGlade:
         for seat_index in xrange(0, 10):
             card_seat = self.glade.get_widget('sit_seat%d' % seat_index)
             (x, y) = (fixed.child_get_property(card_seat, 'x'), fixed.child_get_property(card_seat, 'y'))
-            fixed.remove(card_seat)
+            #fixed.remove(card_seat)
             for card_index in xrange(1, 8):
                 image = gtk.Image()
                 image.set_name("card%d_seat%d" % (card_index, seat_index))
