75b89044e8a3a42f1063f734edcffbd8b2865935
loic <loic@c4f34931-41c7-4605-b615-9bb9f479c57e>
2007-04-19 17:01:51 +0000

    pygame.mixer mockup with control over init failure
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@2721 c4f34931-41c7-4605-b615-9bb9f479c57e

--- tests/test-pokerdisplay2d/pygame.py
+++ tests/test-pokerdisplay2d/pygame.py
@@ -1,1 +1,29 @@
-throw UserWarning, "FAILURE"
+#
+# Copyright (C) 2007 Loic Dachary <loic@dachary.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+class MixerMockup:
+    def __init__(self):
+        self.init_fail = False
+        
+    def init(self):
+        if self.init_fail:
+            raise UserWarning, "pygame.MixerMockup init failed"
+
+    def quit(self):
+        pass
+
+mixer = MixerMockup()
