18f2fc29d791f3340002848d6476ec9c0ee1e886
loic <loic@c4f34931-41c7-4605-b615-9bb9f479c57e>
2008-07-22 11:02:36 +0000

    add locale field to user table
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@4142 c4f34931-41c7-4605-b615-9bb9f479c57e

--- database/1.6.0-1.7.0.sql
+++ database/1.6.0-1.7.0.sql
@@ -3,3 +3,7 @@ UPDATE server SET version = '1.7.0';
 -- Larger field for skin url 
 --
 ALTER TABLE `users` CHANGE COLUMN `skin_url` `skin_url` VARCHAR(128);
+--
+-- Server side, per user locale
+-- 
+ALTER TABLE `users` ADD COLUMN `locale` VARCHAR(32) DEFAULT "en";
