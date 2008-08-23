575ed84a55d02592f6a747bc2a134bb011c52641
loic <loic@c4f34931-41c7-4605-b615-9bb9f479c57e>
2008-08-23 09:03:49 +0000

    . fill the pokertables table to include all fields needed for the PokerTable packet . add resthost_serial to tourneys + tourneys schedule, which is redundant with the route table but speedup requests . index the state of a tourney and the rank of user2tourney for speed . add the resthost table for each table to get a route serial and declare the URL to which it can be reached . add a route table that links table or tourney serials to the resthost that owns them
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@4319 c4f34931-41c7-4605-b615-9bb9f479c57e

--- database/1.6.0-1.7.0.sql
+++ database/1.6.0-1.7.0.sql
@@ -8,6 +8,19 @@ ALTER TABLE `users` CHANGE COLUMN `skin_url` `skin_url` VARCHAR(128);
 -- 
 ALTER TABLE `users` ADD COLUMN `locale` VARCHAR(32) DEFAULT "en";
 --
+-- Tournament state is used for tourneySelect
+--
+ALTER TABLE `tourneys` ADD KEY ( `state` ) ;
+--
+-- Tournaments may be owned by a server.
+--
+ALTER TABLE `tourneys` ADD COLUMN resthost_serial INT UNSIGNED DEFAULT 0 NOT NULL;
+ALTER TABLE `tourneys_schedule` ADD COLUMN resthost_serial INT UNSIGNED DEFAULT 0 NOT NULL;
+--
+-- Tournament rank is used for tourneySelect
+--
+ALTER TABLE `user2tourney` ADD KEY ( `rank` ) ;
+--
 -- History of monitor events
 --
 CREATE TABLE monitor (
@@ -20,10 +33,59 @@ CREATE TABLE monitor (
   PRIMARY KEY (serial, created)
 ) ENGINE=MyISAM;
 --
--- A table that belongs to a tourney is marked as such
+-- Throw away the tables instead of upgrading the table because 
+-- most of it is new anyway and there is no persistent information there.
 --
-ALTER TABLE `pokertables` ADD COLUMN tourney_serial INT UNSIGNED DEFAULT 0 NOT NULL;
---
--- Larger field for table name and unique constraint
---
-ALTER TABLE `pokertables` CHANGE COLUMN `name` `name` VARCHAR(255) NOT NULL UNIQUE;
+DROP TABLE IF EXISTS pokertables;
+
+CREATE TABLE pokertables (
+  serial INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  resthost_serial INT UNSIGNED DEFAULT 0 NOT NULL,
+  seats TINYINT DEFAULT 10,
+  average_pot INT UNSIGNED DEFAULT 0,
+  hands_per_hour INT UNSIGNED DEFAULT 0,
+  percent_flop TINYINT DEFAULT 0,
+  players TINYINT DEFAULT 0,
+  observers INT UNSIGNED DEFAULT 0,
+  waiting TINYINT DEFAULT 0,
+  player_timeout INT UNSIGNED DEFAULT 60,
+  muck_timeout INT UNSIGNED DEFAULT 5,
+  currency_serial INT NOT NULL,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  variant VARCHAR(255) NOT NULL,
+  betting_structure VARCHAR(255) NOT NULL,
+  skin VARCHAR(255) DEFAULT "default" NOT NULL,
+  tourney_serial INT UNSIGNED DEFAULT 0 NOT NULL,
+
+  PRIMARY KEY (serial),
+  KEY (name),
+  KEY (variant),
+  KEY (resthost_serial)
+)  ENGINE=InnoDB CHARSET=utf8;
+
+DROP TABLE IF EXISTS resthost;
+
+CREATE TABLE resthost (
+  serial INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255),
+  host VARCHAR(255),
+  port INT UNSIGNED,
+  path VARCHAR(255),
+
+  PRIMARY KEY (serial)
+) ENGINE=MEMORY;
+
+DROP TABLE IF EXISTS route;
+
+CREATE TABLE route (
+  table_serial INT UNSIGNED,
+  tourney_serial INT UNSIGNED,
+  modified INT UNSIGNED,
+  resthost_serial INT UNSIGNED,
+  
+  PRIMARY KEY (table_serial, tourney_serial),
+  KEY (table_serial),
+  KEY (tourney_serial),
+  KEY (modified),
+  KEY (resthost_serial)
+) ENGINE=MEMORY;
