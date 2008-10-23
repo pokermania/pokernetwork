230f75ea62542a986f781839675aafe2d12f6e2a
loic <loic@c4f34931-41c7-4605-b615-9bb9f479c57e>
2008-10-23 15:36:38 +0000

    call update when the player moves or the hand may not be dealt in a heads up tournament table.
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@4769 c4f34931-41c7-4605-b615-9bb9f479c57e

--- pokernetwork/pokertable.py
+++ pokernetwork/pokertable.py
@@ -1074,6 +1074,7 @@ class PokerTable:
             client.join(other_table)
         other_table.movePlayerTo(serial, money, sit_out)
         other_table.sendNewPlayerInformation(serial)
+        other_table.update()
         if self.factory.verbose:
             self.message("player %d moved from table %d to table %d" % ( serial, game.id, to_game_id ))
 
