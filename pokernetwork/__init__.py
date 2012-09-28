e292e725b61c207110e89a4e8950979ad3621b70
Olaf Gladis <olaf@pokermania.de>
2012-09-28 12:28:32 +0200

    poker-network (2.2.2-5) pokermania; urgency=low

--- pokernetwork/__init__.py
+++ pokernetwork/__init__.py
@@ -2,4 +2,5 @@ import reflogging
 log = reflogging.Logger('pokernetwork')
 
 version_number = "2.2.2"
-protocol_number = "2.2.2"
+# protocol_number is version number of pokerpackets
+protocol_number = "2.2.0"
