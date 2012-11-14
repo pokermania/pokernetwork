d3e63466948db5dd1712fc32d5da992c343fc699
Hannes Uebelacker <hannes@pokermania.de>
2012-11-14 01:04:59 +0100

    updated network build

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, os
+import os, stat
+from stat import S_IRWXU, S_IRWXG, S_IROTH, S_IXOTH
+from glob import glob
 from distutils.core import setup
 
 from distutils.command.build import build as DistutilsBuild
@@ -9,16 +11,33 @@ from distutils.command.build import build as DistutilsBuild
 class ExtendedBuild(DistutilsBuild):
     
     def run(self):
+        os.system("python setup.py configure -b")
+        for f in EXECUTABLES:
+            os.chmod(f, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)
         DistutilsBuild.run(self)
-#        os.system("make -C po all")
-#        os.system("make -C conf buildconf")
+
+EXECUTABLES = [
+    'database/pokerdatabaseupgrade',
+    'pokernetwork/pokerserver',
+    'pokernetwork/pokerbot'
+]
 
 setup(
-    name = 'poker-network',
-    version = '2.2.3',
-    packages = ['pokernetwork', 'twisted/plugins'],
-    cmdclass = {
-    	'build': ExtendedBuild
-    }
+    name='poker-network',
+    version='2.2.3',
+    packages=['pokernetwork'],
+    package_data={'pokernetwork': ['../twisted/plugins/*.py']},
+    data_files=[
+        ('bin', EXECUTABLES),
+        ('share/poker-network', glob('database/*.sql')),
+        ('share/poker-network/conf', glob('conf/*.xml') + ['conf/poker.pem']),
+        ('share/man/man8', [
+            'pokernetwork/pokerserver.8',
+            'pokernetwork/pokerbot.8',
+            'database/pokerdatabaseupgrade.8'
+        ]),
+        ('share/man/man5', ['database/pokerdatabase.5'])
+    ],
+    cmdclass={'build': ExtendedBuild}
 )
 
