9061490f4184a4a650b46d66dc5c816d1acaa846
Olaf Gladis <olaf@pokermania.de>
2012-12-14 17:34:32 +0100

    poker-network (2.3.0-1) pokermania; urgency=low

--- pokernetwork/__init__.py
+++ pokernetwork/__init__.py
@@ -1,6 +1,6 @@
 import reflogging
 log = reflogging.root_logger.get_child('pokernetwork')
 
-version_number = "2.2.3"
+version_number = "2.3.0"
 # protocol_number is version number of pokerpackets
-protocol_number = "2.2.0"
+protocol_number = "2.3.0"
