6ac1ebaaa3135421648265f19a7940fdbfcaf0d7
Olaf Gladis <olaf@pokermania.de>
2013-03-18 11:35:32 +0100

    new protocoll number

--- pokernetwork/__init__.py
+++ pokernetwork/__init__.py
@@ -3,4 +3,4 @@ log = reflogging.root_logger.get_child('pokernetwork')
 
 version_number = "2.3.0"
 # protocol_number is version number of pokerpackets
-protocol_number = "2.3.0"
+protocol_number = "3.0.0"
