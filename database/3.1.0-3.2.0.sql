c892b4d26fa89c3195482a0566a16812be3fa41d
Andor Goetzendorff <goetzend@cs.tum.edu>
2013-04-25 14:54:59 +0200

    there is no table_serial in the hands table

--- database/3.1.0-3.2.0.sql
+++ database/3.1.0-3.2.0.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `pokernetwork`.`hands` DROP `name`, ADD COLUMN `game_id` INT(10) UNSIGNED NULL DEFAULT NULL  AFTER `serial` , ADD COLUMN `tourney_serial` INT(10) UNSIGNED NULL DEFAULT NULL  AFTER `table_serial` ;
+ALTER TABLE `pokernetwork`.`hands` DROP `name`, ADD COLUMN `game_id` INT(10) UNSIGNED NULL DEFAULT NULL  AFTER `serial` , ADD COLUMN `tourney_serial` INT(10) UNSIGNED NULL DEFAULT NULL AFTER `serial` ;
