af4e49a2b3bc46e712a5ef5199f32880eb0b0f4b
Andor Goetzendorff <goetzend@cs.tum.edu>
2013-07-17 19:02:08 +0200

    remove database name from migration

--- database/4.3.0-4.3.1.sql
+++ database/4.3.0-4.3.1.sql
@@ -1,3 +1,3 @@
 -- increased possible betting_structure file name length
-ALTER TABLE `pokernetwork`.`tourneys_schedule` CHANGE COLUMN `betting_structure` `betting_structure` VARCHAR(255) NOT NULL COMMENT 'Betting structure (level-001, level-10-15-pot-limit, level-10-20_200-2000_no-limit, level-15-30-no-limit, level-1-2_20-200_limit)'  ;
-ALTER TABLE `pokernetwork`.`tourneys` CHANGE COLUMN `betting_structure` `betting_structure` VARCHAR(255) NOT NULL  ;
+ALTER TABLE `tourneys_schedule` CHANGE COLUMN `betting_structure` `betting_structure` VARCHAR(255) NOT NULL COMMENT 'Betting structure (level-001, level-10-15-pot-limit, level-10-20_200-2000_no-limit, level-15-30-no-limit, level-1-2_20-200_limit)'  ;
+ALTER TABLE `tourneys` CHANGE COLUMN `betting_structure` `betting_structure` VARCHAR(255) NOT NULL  ;
