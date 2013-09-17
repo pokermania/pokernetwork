91584259fc60de342133b59ef1ce6111e070b4f7
Olaf Gladis <olaf@pokermania.de>
2013-09-17 08:58:02 +0200

    added TODO

--- pokernetwork/pokerservice.py
+++ pokernetwork/pokerservice.py
@@ -759,6 +759,7 @@ class PokerService(service.Service):
         for tourney in filter(lambda tourney: tourney.sit_n_go == 'y', self.tourneys.values()):
             if tourney.state == TOURNAMENT_STATE_REGISTERING and tourney.last_registered is not None and now - tourney.last_registered > self.sng_timeout:
                 tourney.changeState(TOURNAMENT_STATE_CANCELED)
+                #TODO: kick humans
 
         # Respawning sit'n'go tournaments
         for schedule in filter(lambda schedule: schedule['respawn'] == 'y' and schedule['sit_n_go'] == 'y', self.tourneys_schedule.values()):
